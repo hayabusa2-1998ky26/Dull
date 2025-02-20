a2c3e5fa00a11aa3d965e14b4311dc8dd0a5d06a
hayabusa2-1998ky26 <d23021ei@shibaurafzk.com>
2025-02-20 13:10:47 +0900

    2025/02/20 make maze by "hole digging method"

--- main.rb
+++ main.rb
@@ -8,6 +8,23 @@ def pass
   n = 0
 end
 
+def screen_convert(putter)
+  clean
+  for y in 0..11 + 2
+    for x in 0..34
+      if y > 1
+        putter[y - 2][x] = " "
+      end
+      if y < 11
+        putter[y][x] = "■"
+      end
+    end
+    clean
+    puts putter
+    sleep(0.05)
+  end
+end
+
 def wait_for_space
   while true
     if keyin == "space"
@@ -55,7 +72,7 @@ def start
         when 1
           clean
           puts("<About Game>", 
-          "Dull これは迷宮から抜け出す迷路ゲームです。", 
+          "Dull これは迷宮から脱出する迷路ゲームです。", 
           "", 
           "操作方法:", 
           "移動 : WASD", 
@@ -103,20 +120,47 @@ def start
   end
 end
 
+def make_maze(x, y)
+  maze = []
+  for ky in 0..y - 1
+    maze.push([])
+    for kx in 0..x - 1
+      maze[ky].push(1)
+    end
+  end
+  mx, my = 1, 1
+  maze[my][mx] = 0
+  while true
+    while true
+      way = [[2, 0], [-2, 0], [0, 2], [0, -2]]
+      can_way = way.select{|a| ((mx == 1 && a[0] == -2) || (my == 1 && a[1] == -2) || (mx == x - 2 && a[0] == 2) || (my == y - 2 && a[1] == 2)) == false && maze[my + a[1]][mx + a[0]] == 1}
+      if can_way == []
+        break
+      end
+      to_way = can_way[rand(can_way.length)]
+      maze[my + to_way[1]][mx + to_way[0]] = 0
+      maze[my + to_way[1] / 2][mx + to_way[0] / 2] = 0
+      mx, my = mx + to_way[0], my + to_way[1]
+    end
+    next_plots = []
+    for ky in 0..y - 1
+      for kx in 0..x - -1
+        if maze[ky][kx] == 0 && ky % 2 == 1 && kx % 2 == 1
+          next_plots.push([kx, ky])
+        end
+      end
+    end
+    if next_plots.length == ((x - 1) / 2) * ((y - 1) / 2)
+      break
+    end
+    next_plot = next_plots[rand(next_plots.length)]
+    mx, my = next_plot[0], next_plot[1]
+  end
+  return maze
+end
+
 $dull_letter = [" " * 35, "       ■■■■■          ■■  ■■", "       ■■  ■■         ■■  ■■", "       ■■  ■■  ■■ ■■  ■■  ■■", "       ■■  ■■  ■■ ■■  ■■  ■■", "       ■■  ■■  ■■ ■■  ■■  ■■", "       ■■■■■    ■■■■  ■■  ■■"]
 start
 putter = $dull_letter + [" " * 35] + $starter.map{|x| x + " " * (35 - x.length)}
-clean
-for y in 0..11 + 2
-  for x in 0..34
-    if y > 1
-      putter[y - 2][x] = " "
-    end
-    if y < 11
-      putter[y][x] = "■"
-    end
-  end
-  clean
-  puts putter
-  sleep(0.05)
-end
+screen_convert(putter)
+nx, ny = 1, 1
